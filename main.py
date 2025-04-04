1b0a04055dbebecd47e1096332e21f1380fe9540
Moonshammy <benjaminmooneyham1@gmail.com>
2025-04-04 14:22:27 -0600

    ver 1.0

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def get_book_test(path_to_file):
         file_contents = f.read()
     return file_contents
 
-def print_report(chars, words):
+def print_report(chars, words, file):
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {file}...")
     print("----------- Word Count ----------")
     print(f"Found {words} total words")
     print("--------- Character Count -------")
@@ -22,10 +22,10 @@ def main():
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-    
+    file = sys.argv[1]
     char_count = get_num_chars(file)
     words = get_num_words(file)
-    print_report(char_count, words)
+    print_report(char_count, words, file)
 
 
 main()
